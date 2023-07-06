4ea39d2b7f9c467f1658b2316b89a1cb3ce74f6f
ghani fauzan <22081010258@student.upnjatim.ac.id>
2023-07-06 13:29:29 +0700

    Develop create Task View

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,11 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/tasks/', [TaskController::class, 'index'])->name('tasks.index');
-
-// Ditambahkan
-Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::prefix('tasks')
+    ->name('tasks.')
+    ->controller(TaskController::class)
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('{id}/edit', 'edit')->name('edit');
+        Route::get('create', 'create')->name('create');
+    });
