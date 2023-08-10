261f7c6531d125e63a92e7cfd9885ab5d557248b
ghani fauzan <22081010258@student.upnjatim.ac.id>
2023-08-10 14:47:35 +0700

    Set middleware for authentication

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->name('home');
+})->name('home')->middleware('auth');
 
 Route::prefix('tasks')
     ->name('tasks.')
+    ->middleware('auth')
     ->controller(TaskController::class)
     ->group(function () {
         Route::get('/', 'index')->name('index');
@@ -28,9 +29,14 @@ Route::prefix('tasks')
 Route::name('auth.')
     ->controller(AuthController::class)
     ->group(function () {
-        Route::get('signup', 'signupForm')->name('signupForm');
-        Route::post('signup', 'signup')->name('signup');
-        Route::get('login', 'loginForm')->name('loginForm');
-        Route::post('login', 'login')->name('login');
-        Route::post('logout', 'logout')->name('logout');
+        Route::middleware('guest')->group(function () {
+            Route::get('signup', 'signupForm')->name('signupForm');
+            Route::post('signup', 'signup')->name('signup');
+            Route::get('login', 'loginForm')->name('loginForm');
+            Route::post('login', 'login')->name('login');
+        });
+
+        Route::middleware('auth')->group(function () {
+            Route::post('logout', 'logout')->name('logout');
+        });
     });
